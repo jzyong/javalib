8fd785c567f79d31e9f3c10df9987c248dd364e4
jzy <123456789>
2022-02-25 15:28:55 +0800

    script

---
+++ javalib-base/src/main/java/com/jzy/javalib/base/script/IInitScript.java
@@ -0,0 +1,20 @@
+package com.jzy.javalib.base.script;
+
+/**
+ * 初始化脚本
+ * 
+ * @author JiangZhiYong
+ * @date 2018/12/19
+ */
+public interface IInitScript extends IScript {
+    /**
+     * 初始化
+     */
+    void init();
+
+    /** 消耗 */
+    default void destroy() {
+
+    }
+
+}
---
+++ javalib-base/src/main/java/com/jzy/javalib/base/script/IScript.java
@@ -0,0 +1,10 @@
+package com.jzy.javalib.base.script;
+
+/**
+ * 脚本标识类
+ * @author JiangZhiYong
+ * @date 2018/12/19
+ */
+public interface IScript {
+
+}
